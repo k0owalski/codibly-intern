2b566cd40c1c8e4093db0a26bf980b89f5f1a049
kowalski <knrd.kowalik@gmail.com>
2023-01-28 17:40:27 +0100

    Creating the root view and the globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import GlobalStyle from 'theme/GlobalStyle';
+import Root from 'views/Root/Root';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Root />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/theme/GlobalStyle.tsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+`;
+
+export default GlobalStyle;
